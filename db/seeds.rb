48d65eb6c3abbbe2c1aab5614df204c86bb345b8
davidtsai130 <davidtsai130@gmail.com>
2016-08-12 13:31:55 -0400

    factory girl/faker gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+FactoryGirl.define do
+  factory :user do
+    first_name            Faker::Name.first_name
+    last_name             Faker::Name.last_name
+    phone                 Faker::PhoneNumber.cell_phone
+    email                 Faker::Internet.email
+    street_address        Faker::
+    city
+    state
+    password              "password"
+    password_confirmation "password"
+  end
+
+  factory :tags do
+    first_name            Faker::Name.first_name
+    last_name             Faker::Name.last_name
+    phone                 Faker::PhoneNumber.cell_phone
+    email                 Faker::Internet.email
+    password              "password"
+    password_confirmation "password"
+  end
+
+  factory :listing do
+    seller_id            Faker::Name.first_name
+    title                Faker::Name.last_name
+    description          Faker::PhoneNumber.cell_phone
+    location             Faker::Internet.email
+    image                 "password"
+    price                 "password"
+    sold?                Faker::
+  end
+
+  factory :organization do
+    name            Faker::Name.first_name
+  end
+
+
+end
