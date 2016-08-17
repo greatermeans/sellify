e19d85d7752145ff0aa0058568f388854997ca36
davidtsai130 <davidtsai130@gmail.com>
2016-08-17 19:23:10 -0400

    added filter for organizations

--- api/app/serializers/user_serializer.rb
+++ api/app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :dashboard_listings
+  attributes :id, :name, :email, :dashboard_listings, :all_listings
   has_many :communities
   has_many :conversations
   has_many :organizations, through: :communities
--- react/src/containers/allListings.js
+++ react/src/containers/allListings.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import auth from './authenticationResource'
+import ListingBox from '../components/listingBox'
 
 const AllListing = class extends Component {
+
+  displayAllListings() {
+    return this.props.user.all_listings.map ( (listing, idx) => {
+      return <ListingBox key={idx} {...listing}/>
+    })
+  }
+
+  createDropdown() {
+    return (
+      <div>
+      <select value="" onChange={this.handleonChange.bind(this)}>
+       <option value="" selected>All Organizations</option>
+        {this.props.user.organizations.map ( (org, idx) => {
+          return <option value={org.id}> {org.name} </option>
+        })}
+       </select>
+      </div>
+    )
+  }
+
+  handleonChange(event) {
+    debugger
+    this.props.user.organizations.filter((org)=>{
+      return org.id === parseInt(event.target.value)
+    })
+  }
+
   render() {
     return (
       <div>
-      HELLOOOOOO
+        <h1> Listings </h1>
+          {this.createDropdown()}
+        <h3> Filter by Organization </h3>
+          {this.displayAllListings()}
       </div>
     )
   }
