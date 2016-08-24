0e3879917a22a1cfa54dac4311bd1b42cf953815
greatermeans <gakouri@gmail.com>
2016-08-24 12:48:15 -0400

    garbage collector

--- react/src/reducers2/organizationReducer.js
+++
@@ -1,9 +0,0 @@
-const orgReducer = (state=[], action)=>{
-	switch(action.type){
-		case 'GET_ORG':
-			return state.concat(action.payload)
-		default:
-			return state
-	}
-}
-export default orgReducer
