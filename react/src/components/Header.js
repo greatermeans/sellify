f053794d68b12d9b84b39a1cb0f977eebbaa643d
yyangflatiron <yichen.yang@flatironschool.com>
2016-08-24 14:58:32 -0400

    fixed conflicts

--- react/src/components/Header.js
+++ react/src/components/Header.js
@@ -29,7 +29,6 @@ class Header extends Component {
           <li><Link to={'/profile'}>{authenticatedUser.name}</Link></li>
           <li><Link to={'/dashboard'}>Dashboard</Link></li>
           <li><Link to={'/organizations'}>My organizations</Link></li>
-          <li><a onClick={this.props.logout}>Log out</a></li>
           <li><Link to={'/'} onClick={this.props.logout}>Log Out</Link></li>
         </ul>
       );
